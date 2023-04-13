874157a61417b78a1e89e28e09783225d5a87984
Nur <90011276+Nur9835@users.noreply.github.com>
2023-04-14 02:30:58 +0300

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-import { ExampleComponent } from 'nur-button-ui'
+import { Button } from 'nur-button-ui'
 import 'nur-button-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return(
+  <div className='buttonApp'>
+    <Button text="Primary Button" type="primary"/>
+    <br/>
+    <Button text="Default Button " type="default"/>
+    <br/>
+    <Button text="Dashed Button" type="dashed"/>
+    <br/>
+    <Button text="Text Button" type="text"/>
+    <br/>
+    <a href="#" classNme="my-button">
+     <Button text="Link Button" type = "link"/></a>
+
+  </div>
+  )
 }
 
 export default App
