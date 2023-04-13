a656ab60655b3ff4a4746ea788b071361d4d55be
Nur <90011276+Nur9835@users.noreply.github.com>
2023-04-14 02:27:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import styles from './styles.module.css'
 
 export const Button = ({type,text}) =>{
   return <button className={`${styles.btn} ${styles[type]}`}>{text}</button>
-
+// her bir butonun tipi(primary, default, dashed, text, link) gelen type propu ile değişmektedir
 }
